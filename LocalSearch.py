12d4d5062a37df2a8be5868fba73302e36006016
Richieleonardo <116273535+Richieleonardo@users.noreply.github.com>
2024-10-03 06:51:14 +0700

    more local search

--- LocalSearch.py
+++ LocalSearch.py
@@ -46,7 +46,7 @@ class HillClimbingSidewayMove(object) : #Jump somewhere else when reaching shoul
     def run(self, init_state):
         state = init_state
         
-        jumplimit = 100
+        jumplimit = 5
         count = 0 # while condition i < jumpcount
         candidate = MagicCube(state.n)
         lowest_violation = 999
@@ -75,9 +75,9 @@ class HillClimbingSidewayMove(object) : #Jump somewhere else when reaching shoul
                 break
             elif (candidate.checkCube() == violation): #reaching 
                 #We make a jump by doing random swap to the magic cube
-                for i in range(5):
-                    neighbour = state.getNeighbour("random")
-                state = neighbour
+                # for i in range(5):
+                #     neighbour = state.getNeighbour("random")
+                state.PartialRand() #randomize 5 number at once
                 count += 1
                 print("Doing sideway jump : ", count, "time")
             else:
@@ -92,11 +92,11 @@ class HillClimbingStochastic(object) :
 
     def run(self, init_state):
         state = init_state
-        violation = state.checkCube()
+        violation = state.checkCube()  
         print("Initial violation : ", violation)
         for i in range (10000):
             neighbour = state.getNeighbour("random")
-            if neighbour.checkCube() > state.checkCube():
+            if neighbour.checkCube() < state.checkCube():
                 state = neighbour
         new_violation = state.checkCube()
         print("New violation : ", new_violation)        
@@ -107,4 +107,37 @@ class HillClimbingRandomRestart(object):
         self.name = "Hill Climbing Random Restart"
 
     def run(self, init_state):
-        return 0
+        state = init_state
+        violation = state.checkCube()
+        
+        restart_limit = 10
+        count = 0
+        tempstate = MagicCube(init_state.n)
+        tempstate.cube = state.cube
+           
+        if(violation == 0):
+            return state
+        print("Initial violation : ", violation)
+        while count < restart_limit:
+            for i in range(10000): #10000 iteration of 10 different initial state
+                neighbour = state.getNeighbour("random")
+                if neighbour.checkCube() < state.checkCube():
+                    state = neighbour
+            if(state.checkCube() < tempstate.checkCube()):
+                tempstate.cube = state.cube
+            print("Random restart : ", count+1)
+            print("Temp violation : ", tempstate.checkCube())
+            state.RandCube()
+            count += 1
+        
+        state = tempstate
+        print("Final state has violation of : ", tempstate.checkCube())    
+        return state
+    
+class GeneticAlgorithm(object):
+    def __init__(self):
+        self.name = "Genetic Algorithm"
+    
+    
+    
+        
